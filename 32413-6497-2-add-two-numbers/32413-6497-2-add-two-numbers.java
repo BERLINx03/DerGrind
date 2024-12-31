ce1561a02fb41f7f11f398bd8da58030d6e44d2d
abdallah <abdallahelsokkary4399@gmail.com>
2024-12-31 19:57:42 +0200

    Time: 1 ms, Memory: 44.5 MB - LeetHub

--- 32413-6497-2-add-two-numbers/32413-6497-2-add-two-numbers.java
+++ 32413-6497-2-add-two-numbers/32413-6497-2-add-two-numbers.java
@@ -14,35 +14,14 @@ class Solution {
         ListNode sum = new ListNode(0);
         ListNode returnedSum = sum;
 
-        while(l1 != null || l2 != null){
-            if(l2 == null){
-                l2 = new ListNode(0);
-            }
-            if (l1 == null){
-                l1 = new ListNode(0);
-            }
-
-            int nodeSum = l1.val + l2.val;
-            if(nodeSum > 9) carrier = nodeSum / 10; else carrier = 0;
-
-            sum.val = (l1.val + l2.val) % 10;
-            if(l1.next != null){
-                l1.next.val = l1.next.val + carrier;
-                sum.next = new ListNode(0);
-                sum = sum.next;
-            } else if(l2.next != null){
-                l2.next.val = l2.next.val + carrier;
-                sum.next = new ListNode(0);
-                sum = sum.next;
-            }
-
-
-            l1 = l1.next;
-            l2 = l2.next;
+        while (l1 != null || l2 != null || carrier != 0){
+            int nodeSum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + carrier;
+            carrier = nodeSum/10;
+            sum.next = new ListNode(nodeSum % 10);
+            sum = sum.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
-        if (carrier != 0) {
-            sum.next = new ListNode(carrier);
-        }
-        return returnedSum;
+        return returnedSum.next;
     }
 }
